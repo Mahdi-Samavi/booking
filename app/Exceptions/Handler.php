e67f4d4ce4dc9ab9079e62cd064d170fe73481a5
Mahdi Samavi <samavi.mahdi@gmail.com>
2022-09-29 00:00:35 +0330

    Exception hanler modified

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -43,8 +46,50 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (Throwable $e, $request) {
+            if (! $request->wantsJson()) {
+                return;
+            }
+
+            if ($e instanceof AccessDeniedHttpException) {
+                return response()->json([
+                    'error' => 'ACCESS_DENIED',
+                    'message' => __('You don\'t have permission for do this action.'),
+                ]);
+            }
         });
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->shouldReturnJson($request, $exception)
+                    ? response()->json([
+                        'error' => 'AUTHORIZATION_FAILED',
+                        'message' => __('Your client credentials don\'t match an clients in system.'),
+                    ], 401)
+                    : redirect()->guest($exception->redirectTo() ?? route('login'));
+    }
+
+    /**
+     * Convert a validation exception into a JSON response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Validation\ValidationException  $exception
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        return response()->json([
+            'error' => 'VALIDATION_ERROR',
+            'message' => __('Your input values has some errors'),
+            'fields' => $exception->errors(),
+        ], 400);
     }
 }
